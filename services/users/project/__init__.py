db91e2c26ead7a34e5b27dba1b5537ae0316b6e4
ivan <sinitsinvanya@gmail.com>
2019-01-12 16:19:29 +0400

    Config testing and user model added.

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -4,17 +4,22 @@ from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
 app_settings = os.getenv('APP_SETTINGS')
 app.config.from_object(app_settings)
 
 db = SQLAlchemy(app)
 
+import sys
+
+print(app.config, file=sys.stderr)
+
 
 class User(db.Model):
-    __tablename_ = 'users'
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(128), nullable=False)
-    email = db.Column(db.String(128), default=True, nullable=False)
+    email = db.Column(db.String(128), nullable=False)
     active = db.Column(db.Boolean(), default=True, nullable=False)
 
     def __init__(self, username, email):
